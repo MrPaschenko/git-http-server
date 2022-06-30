04008ab52a7ff16fc3a429cfbd7b78178575d8aa
mrpaschenko <paschenkod2@gmail.com>
2022-06-30 13:11:58 +0300

    Move getTime function to server.go

--- server.go
+++ server.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/time", getTime)
 
-	fmt.Printf("Starting server on ")
+	fmt.Printf("Starting server on 8795")
 	if err := http.ListenAndServe(":8795", nil); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func getTime(w http.ResponseWriter, req *http.Request) {
+	currentTime := time.Now()
+	timeWithTimestamp := currentTime.Format(time.RFC3339)
+	data := map[string]string{
+		"time": timeWithTimestamp,
+	}
+	b, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if req.Method == "GET" {
+		fmt.Fprintf(w, string(b))
+	} else {
+		fmt.Fprintf(w, "Sorry, only GET request is supported.")
 	}
 }
